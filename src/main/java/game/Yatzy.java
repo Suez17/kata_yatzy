67c4264ac0877068a8f2705d3fc744f4ebd0e585
Suez17 <k-michael@hotmail.fr>
2021-03-22 15:29:43 +0100

    Refactoring fourOfAKind

--- src/main/java/game/Yatzy.java
+++ src/main/java/game/Yatzy.java
@@ -74,18 +74,16 @@ public class Yatzy {
         return sumOfThreeOfAKind;
     }
 
-    public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5) {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[_1 - 1]++;
-        tallies[_2 - 1]++;
-        tallies[d3 - 1]++;
-        tallies[d4 - 1]++;
-        tallies[d5 - 1]++;
-        for (int i = 0; i < 6; i++)
-            if (tallies[i] >= 4)
-                return (i + 1) * 4;
-        return 0;
+    public static int fourOfAKind(int d1, int d2, int d3, int d4, int d5) {
+        int sumOfThreeOfAKind = 0;
+        final List<Integer> dices = Arrays.asList(d1, d2, d3, d4, d5);
+        final Set<Integer> duplicateDiceValues = getDuplicateDiceValues(d1, d2, d3, d4, d5);
+        for (int duplicateDiceValue : duplicateDiceValues) {
+            if (dices.stream().filter(d -> d == duplicateDiceValue).count() >= 4) {
+                sumOfThreeOfAKind += duplicateDiceValue * 4;
+            }
+        }
+        return sumOfThreeOfAKind;
     }
 
     public static int smallStraight(int d1, int d2, int d3, int d4, int d5) {
--- src/test/java/game/YatzyTest.java
+++ src/test/java/game/YatzyTest.java
@@ -197,10 +197,33 @@ public class YatzyTest {
     }
 
     @Test
-    public void four_of_a_knd() {
-        assertEquals(12, Yatzy.four_of_a_kind(3, 3, 3, 3, 5));
-        assertEquals(20, Yatzy.four_of_a_kind(5, 5, 5, 4, 5));
-        //assertEquals(9, Yatzy.four_of_a_kind(3, 3, 3, 3, 3));
+    public void test_fourOfAKind_scores_12() {
+        assertEquals(12, Yatzy.fourOfAKind(3, 3, 3, 3, 5));
+    }
+
+    @Test
+    public void test_fourOfAKind_scores_20() {
+        assertEquals(20, Yatzy.fourOfAKind(5, 5, 5, 4, 5));
+    }
+
+    @Test
+    public void test_fourOfAKind_scores_12_when_five_of_a_kind() {
+        assertEquals(12, Yatzy.fourOfAKind(3, 3, 3, 3, 3));
+    }
+
+    @Test
+    public void test_fourOfAKind_scores_0_when_full_house() {
+        assertEquals(0, Yatzy.fourOfAKind(3, 3, 3, 5, 5));
+    }
+
+    @Test
+    public void test_fourOfAKind_scores_0_when_three_of_a_kind() {
+        assertEquals(0, Yatzy.fourOfAKind(3, 3, 3, 4, 5));
+    }
+
+    @Test
+    public void test_fourOfAKind_scores_0_when_pair() {
+        assertEquals(0, Yatzy.fourOfAKind(3, 3, 2, 4, 5));
     }
 
     @Test

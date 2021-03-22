e8c81db9b11b2aadd08d3d44cd6971370a8f796e
Suez17 <k-michael@hotmail.fr>
2021-03-22 18:08:43 +0100

    Refactoring

--- src/main/java/game/Yatzy.java
+++ src/main/java/game/Yatzy.java
@@ -8,7 +8,7 @@ import java.util.stream.Stream;
 public class Yatzy {
 
     public static int chance(int d1, int d2, int d3, int d4, int d5) {
-        return d1 + d2 + d3 + d4 + d5;
+        return (d1 + d2 + d3 + d4 + d5);
     }
 
     public static int yatzy(int d1, int d2, int d3, int d4, int d5) {

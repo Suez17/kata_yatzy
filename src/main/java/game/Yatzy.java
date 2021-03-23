2f29e626b676d0e9ced9a280bd7af6137d6e9e12
Suez17 <k-michael@hotmail.fr>
2021-03-23 08:27:45 +0100

    Refactoring methods name

--- src/main/java/game/Yatzy.java
+++ src/main/java/game/Yatzy.java
@@ -40,12 +40,12 @@ public class Yatzy {
     }
 
     public static int pair(int d1, int d2, int d3, int d4, int d5) {
-        return getMaxValue(getDuplicateDiceValues(d1, d2, d3, d4, d5)) * 2;
+        return findMaxValue(findDuplicateDiceValues(d1, d2, d3, d4, d5)) * 2;
     }
 
     public static int twoPairs(int d1, int d2, int d3, int d4, int d5) {
         int sumOfTwoPairs = 0;
-        if (getDuplicateDiceValues(d1, d2, d3, d4, d5).size() == 2) {
+        if (findDuplicateDiceValues(d1, d2, d3, d4, d5).size() == 2) {
             sumOfTwoPairs = sumOfAKind(2, false, d1, d2, d3, d4, d5);
         }
         return sumOfTwoPairs;
@@ -61,18 +61,18 @@ public class Yatzy {
 
     public static int smallStraight(int d1, int d2, int d3, int d4, int d5) {
         final String smallStraight = "12345";
-        return smallStraight.equals(getOrderedDices(d1, d2, d3, d4, d5)) ? 15 : 0;
+        return smallStraight.equals(createOrderedChainOfDice(d1, d2, d3, d4, d5)) ? 15 : 0;
     }
 
     public static int largeStraight(int d1, int d2, int d3, int d4, int d5) {
         final String largeStraight = "23456";
-        return largeStraight.equals(getOrderedDices(d1, d2, d3, d4, d5)) ? 20 : 0;
+        return largeStraight.equals(createOrderedChainOfDice(d1, d2, d3, d4, d5)) ? 20 : 0;
     }
 
     public static int fullHouse(int d1, int d2, int d3, int d4, int d5) {
         int sumOfFullHouse = 0;
 
-        if (getDuplicateDiceValues(d1, d2, d3, d4, d5).size() == 2) {
+        if (findDuplicateDiceValues(d1, d2, d3, d4, d5).size() == 2) {
             sumOfFullHouse = sumOfAKind(3, true, d1, d2, d3, d4, d5);
             if (sumOfFullHouse != 0) {
                 sumOfFullHouse += sumOfAKind(2, true, d1, d2, d3, d4, d5);
@@ -85,7 +85,7 @@ public class Yatzy {
                                   int d1, int d2, int d3, int d4, int d5) {
         int sumOfAKind = 0;
         final List<Integer> dices = Arrays.asList(d1, d2, d3, d4, d5);
-        final Set<Integer> duplicateDiceValues = getDuplicateDiceValues(d1, d2, d3, d4, d5);
+        final Set<Integer> duplicateDiceValues = findDuplicateDiceValues(d1, d2, d3, d4, d5);
         for (int duplicateDiceValue : duplicateDiceValues) {
             final long totalDuplicateValues = countDuplicateValues(duplicateDiceValue, dices);
             if ((!strictComparison && totalDuplicateValues >= valueOfAKind) ||
@@ -108,21 +108,21 @@ public class Yatzy {
                 .count();
     }
 
-    private static int getMaxValue(Set<Integer> set) {
+    private static int findMaxValue(Set<Integer> set) {
         return set.stream()
                 .mapToInt(Integer::valueOf)
                 .max()
                 .orElse(0);
     }
 
-    private static Set<Integer> getDuplicateDiceValues(int d1, int d2, int d3, int d4, int d5) {
+    private static Set<Integer> findDuplicateDiceValues(int d1, int d2, int d3, int d4, int d5) {
         final Set<Integer> set = new HashSet<>();
         return Stream.of(d1, d2, d3, d4, d5)
                 .filter(d -> !set.add(d))
                 .collect(Collectors.toSet());
     }
 
-    private static String getOrderedDices(int d1, int d2, int d3, int d4, int d5) {
+    private static String createOrderedChainOfDice(int d1, int d2, int d3, int d4, int d5) {
         return IntStream.of(d1, d2, d3, d4, d5)
                 .sorted()
                 .mapToObj(String::valueOf)
